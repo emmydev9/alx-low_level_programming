f13cff206633c10f7f54ce5179c86ddfdc49bf36
Emmy <softdevcoder@gmail.com>
2022-04-07 15:28:10 -0400

    print a sentence using printf function in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main -  Entry point of the program
+* @void: contains no argument
+* Return: return 0 if no error occurs
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
