275569c29b79744e452a91fbb2f456209c1a8ccd
Emmy <softdevcoder@gmail.com>
2022-04-08 16:32:56 -0400

    prints int 0-9 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry points
+* @void: No argument required
+* Return: returns 0 if success
+**/
+int main(void)
+{
+	int i = 48;
+
+	for (; i < 58; i++)
+{
+	putchar(i);
+}
+	putchar('\n');
+	return (0);
+}
