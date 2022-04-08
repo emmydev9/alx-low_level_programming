ffcc331109c3aa2fd2783497788cb431c45d97e2
Emmy <softdevcoder@gmail.com>
2022-04-08 17:14:06 -0400

    created 9-print_comb.c hope it works (Oo)

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry Point function
+* @void: No arguments Needed
+* Return: returns 0 if succes
+**/
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i <= 57; i++)
+{
+	putchar(i);
+	putchar(',');
+	putchar(' ');
+}
+	putchar('\n');
+	return (0);
+}
