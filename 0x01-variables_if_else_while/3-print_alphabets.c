6fa96d62fba5c7783631a5a5b85d259f2a1365d0
Emmy <softdevcoder@gmail.com>
2022-04-08 15:37:19 -0400

    both upper and lowercase letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+* main - Entry points
+* @void: No arguments
+* Return: returns 0 if success
+**/
+int main(void)
+{
+	char aph;
+
+	for (aph = 'a'; aph <= 'z'; aph++)
+		putchar(aph)
+
+	for (aph = 'A'; aph <= 'Z'; aph++)
+		putchar(aph)
+
+	putchar('\n')
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@
 #check whether a random generated number is positive, negative or zero -> 0-positive_or_nagative.c
 To get the last digits of a random int value -> 1-last_digit.c
 #prints all the letter of the alphabets in lowercase
+#prints both upper and lower of all alphabets -> 3-print_alphabets.c
