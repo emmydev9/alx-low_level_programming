82732f0413c6f64a63ba8ba2c3a792d9a1ac6148
Emmy <softdevcoder@gmail.com>
2022-04-08 17:42:37 -0400

    created 100-print_comb3.c hope it works

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry points Function
+* @void: No arguments needed
+* Return: return 0 if success
+**/
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 57; i++)
+{
+	for (j = i + 1; j <= 57; j++)
+{
+	putchar(i);
+	putchar(j);
+	if (i != 57)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+	putchar('\n');
+	return (0);
+}
