17c0aa6828f88fc5afa30e5a8951ed48ded63b9e
Emmy <softdevcoder@gmail.com>
2022-04-12 16:08:25 -0400

    created 2-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,11 +3,15 @@
 * print_alphabet - print alphabets to the stdout
 * @void: takes no argument
 **/
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	int i;
-	for (i = 97; i <= 122; i++)
-		putchar(i);
+	int i, j;
+	for (i = 0; i < 10; i++)
+{
+	for (j = 97; j <= 122; j++)
+		putchar(j);
 
 	putchar('\n');
+}
+
 }
