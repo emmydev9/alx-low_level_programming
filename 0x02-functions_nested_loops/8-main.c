85a1ec502c29546abf6527094f6b1fea3d2d2746
Emmy <softdevcoder@gmail.com>
2022-04-12 18:26:26 -0400

    created 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdio.h"
+/**
+* main - Entry point
+* @void: No argument needed
+* Return: returns 0 if no error occured
+**/
+int main(void)
+{
+    jack_bauer();
+    return (0);
+}
