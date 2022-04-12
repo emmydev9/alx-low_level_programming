23f7d1610d0a854bcf1974aa9af7c858cf224003
Emmy <softdevcoder@gmail.com>
2022-04-12 17:41:07 -0400

    created 5-sign.c

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* main - Entry point
+* @void: No argument needed
+* Return: returns 0 if no error occured
+**/
+int main(void)
+{
+    int r;
+
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
+}
