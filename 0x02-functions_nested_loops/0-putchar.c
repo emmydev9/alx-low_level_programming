8cfe8c2541a3336f0722a7e2fe2d911384a1d5ce
Emmy <softdevcoder@gmail.com>
2022-04-12 11:56:56 -0400

    create a file 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* main - Entry point function
+* @void: takes no argument
+* Return: returns 0 if success
+**/
+int main(void)
+{
+	char _putchar[8] = "_putchar";
+
+	printf("%s", _putchar);
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#dealing with functions and nested loops
+#print the string _putchar -> 0-putchar.c
