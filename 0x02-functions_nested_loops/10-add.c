cd82a8550ba457833f34892292f1738acd4f4067
Emmy <softdevcoder@gmail.com>
2022-04-12 19:15:58 -0400

    added 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
+/**
+* add - print alphabets to the stdout
+* @a: takes int a as argument
+* @b: takes int b as argument
+* Return: void
+**/
+int add(int a, int b)
+{
+	return (a+b);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "stdio.h"
+/**
+* main - Entry point
+* @void: No argument needed
+* Return: returns 0 if no error occured
+**/
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
