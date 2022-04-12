7fcd79b140888830b70e081b2314e9f93352e669
Emmy <softdevcoder@gmail.com>
2022-04-12 18:00:29 -0400

    added a fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
 /**
 * _abs - print alphabets to the stdout
 * @n: takes an integer argument
@@ -6,12 +8,5 @@
 **/
 int  _abs(int n)
 {
-	if (n > 0)
-		return (1);
-	else if (n == 0)
-		return (0);
-	else
-	return (n * -1);
-
-_putchar('\n');
+	return abs(n);
 }
