c98c957f9cc1cd49baa499c8a553e180cf3a73eb
Emmy <softdevcoder@gmail.com>
2022-04-12 19:07:50 -0400

    added betty style indentation

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,15 +19,15 @@ void times_table(void)
 			_putchar(44);
 			_putchar(32);
 			if (z <= 9)
-{
-	_putchar(32);
-	_putchar(z + 48);
-}
-else
-{
-	_putchar((z / 10) + 48);
-	_putchar((z % 10) + 48);
-}
+			{
+				_putchar(32);
+				_putchar(z + 48);
+			}
+			else
+			{
+				_putchar((z / 10) + 48);
+				_putchar((z % 10) + 48);
+			}	
 		}
 		_putchar('\n');
 	}
