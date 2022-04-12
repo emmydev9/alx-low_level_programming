96cd4044d6b938a270efb99287bb2b06c3a76ebb
Emmy <softdevcoder@gmail.com>
2022-04-12 16:28:08 -0400

    created 3-main.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,15 +3,16 @@
 * print_alphabet - print alphabets to the stdout
 * @void: takes no argument
 **/
-void print_alphabet_x10(void)
+int  _islower(int c)
 {
-	int i, j;
-	for (i = 0; i < 10; i++)
+	if (c >= 97 && c <= 122)
 {
-	for (j = 97; j <= 122; j++)
-		putchar(j);
-
-	putchar('\n');
+	return (1);
 }
+	else
+{
+	return (0);
+}
+	putchar('\n');
 
 }
