f32104a1106f469c8e9dd1b24b0d51f8f7c58744
Emmy <softdevcoder@gmail.com>
2022-04-12 20:30:26 -0400

    created 101-natural.c file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point function
+* @void: no argument needed
+* Return: success
+**/
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 || i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
