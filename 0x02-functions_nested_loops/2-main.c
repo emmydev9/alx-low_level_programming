ea6f432cb99d1a9699a341753797cb22139eac62
Emmy <softdevcoder@gmail.com>
2022-04-12 21:36:05 -0400

    made some adjusts

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,19 +1,12 @@
 #include "main.h"
+
 /**
-* main - Entry point
-* @void: No argument needed
-* Return: returns 0 if no error occured
-**/
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
+    print_alphabet_x10();
     return (0);
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
-void print_alphabet_x10(void);
+#include "main.h"
+/**
+* print_alphabet_x10 - print in 10 lines
+*
+*
+**/
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
