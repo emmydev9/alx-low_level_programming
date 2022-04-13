7a066dba922c1c470747273d13a02ad210dc96ec
Emmy <softdevcoder@gmail.com>
2022-04-13 16:07:37 -0400

    created 0-main.c and main.h file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - Entry Points 
+* @void: No arguments needed
+* Return: return 0 on success
+**/
+int main(void)
+{
+		int i;
+
+	if (positive_or_negative(i) == 0)
+			printf("%d is zero", i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+#full stack development journey with ALX - Debugging
