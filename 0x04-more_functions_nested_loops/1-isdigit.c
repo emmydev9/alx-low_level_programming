f9ac958d7ae4e0b272a43e8e30ba6b8b93b5140c
Emmy <softdevcoder@gmail.com>
2022-04-14 11:29:11 -0400

    created 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _isdigit - check digit input
+* @c: integer argument
+* Return: int
+**/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+
+	else
+		return (0)
+}
