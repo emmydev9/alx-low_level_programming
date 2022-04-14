a9cf076c000052c2d74da6ef6f545358f61f75a1
Emmy <softdevcoder@gmail.com>
2022-04-14 13:38:47 -0400

    created 7-print_diagonal.c file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_diagonal - print diagonally
+* @n: int n
+**/
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(32);
+		}
+		if (i < (n -1 ))
+			_putchar(92);
+
+		_putchar('\n');
+	}
+}
