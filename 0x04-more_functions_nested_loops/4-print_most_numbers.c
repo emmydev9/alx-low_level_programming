ad3aaa16253bc4bff2e7e43f2ebf22235afd8989
Emmy <softdevcoder@gmail.com>
2022-04-14 12:12:36 -0400

    created 4-print_most_numbers.c file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_most_numbers - Entry point functions
+* @void: no argument needed
+**/
+int print_most_numbers(void)
+{
+	int i;
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
