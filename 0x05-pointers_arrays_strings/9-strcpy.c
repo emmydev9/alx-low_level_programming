a4ac36817d89593c10775e150619ebb65b9e6926
Emmy <softdevcoder@gmail.com>
2022-04-20 17:18:10 -0400

    added 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strcpy - a pointer to a function
+* @dest: destination arg
+* @src: source arg
+* Return: dest
+**/
+char *_strcpy(char *dest, char *src)
+{
+	int strlen, i;
+
+	strlen = 0;
+
+	while (*(src + strlen))
+		strlen++;
+
+	for (i = 0; i < strlen; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
