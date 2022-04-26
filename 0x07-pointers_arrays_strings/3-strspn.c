49bc5a2d93687827610e655203af8fb0f212d549
Emmy <softdevcoder@gmail.com>
2022-04-26 13:11:27 -0400

    added 3-strspn.c file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strspn - lenght of segment
+* @s: string.
+* @accept: segment to find
+* Return: unsigned int
+**/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, test;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		test = 1;
+		for (j = 0; *(accept + j) != '\0';j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				test = 0;
+				break;
+			}
+		}
+		if (test == 1)
+			break;
+	}
+	return (i);
+
+}
