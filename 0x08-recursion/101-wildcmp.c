3809b538d4ef5d27fe7f0967b0d4f75d1519cf4d
Emmy <softdevcoder@gmail.com>
2022-04-29 04:12:56 -0400

    added 101-wildcmp.c file

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* wildcmp - compares two strings and returns 1 or 0.
+* @s1: main string.
+* @s2: string to be compared with.
+* Return: int.
+**/
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+		return (0);
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
