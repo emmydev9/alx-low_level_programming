97377aa94ab69ba2cb8d8152dbc9f43de06a5ee9
Emmy <softdevcoder@gmail.com>
2022-05-02 09:45:33 -0400

    added 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry points
+* @argc: number of agruments
+* @argv: pointer to an array of char.
+* Return: Always success
+**/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
