edf2a487abb79ff4ffc498f696394260d2b61072
Emmy <softdevcoder@gmail.com>
2022-05-02 09:33:54 -0400

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry points function
+* @argc: number of arguments
+* @argv: pointer to an array of char.
+* Return: Always success dear
+**/
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
