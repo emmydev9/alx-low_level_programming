05e3edff35551be50ebaa22f43d67c2f38410baa
Emmy <softdevcoder@gmail.com>
2022-05-06 17:22:42 -0400

    added NULL checker

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,13 @@ char *create_array(unsigned int size, char c)
 	char *buffer;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	buffer = malloc(sizeof(char) * size);
+
+	if (buffer == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
