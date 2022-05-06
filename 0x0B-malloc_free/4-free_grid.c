cfe894c0e56f5916e337ccde6c2c0cea28b10056
Emmy <softdevcoder@gmail.com>
2022-05-06 18:42:48 -0400

    added 4-free_grid.c file

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* free_grid - free grid
+* @grid: pointer to a pointer int.
+* @height: int
+**/
+
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+		{
+			free(grid[height]);
+		}
+		free(height);
+	}
+}
