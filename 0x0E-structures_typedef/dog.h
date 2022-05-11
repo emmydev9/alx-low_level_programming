0bed0edc1d00f5a7d8e8ad58b3d38e825b3cdca1
Emmy <softdevcoder@gmail.com>
2022-05-11 14:23:21 -0400

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - dog characteristics
+ * @name: first member
+ * @age: second member
+ * @owner: Third member
+ *
+ * Description: a struct about dog characteristics and owner.
+**/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
