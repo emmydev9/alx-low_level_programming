14c1408daeed0556092620284f0e24b92de20cc7
Emmy <softdevcoder@gmail.com>
2022-05-11 15:11:07 -0400

    added a conditional statement for age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,10 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
+		if (!(d->age))
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("owner: (nil)\n");
 		else
