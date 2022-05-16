06b81e65ed8359262eb7640380c0c5f1158ffb47
Emmy <softdevcoder@gmail.com>
2022-05-16 17:05:30 -0400

    remove condition for n

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,17 +11,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i, pointer;
 	va_list ptr;
 
-	if (n)
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(ptr, n);
-		for (i = 0; i < n; i++)
-		{
-			pointer = va_arg(ptr, unsigned int);
-			printf("%d", pointer);
-			if (i != n - 1 && separator != NULL)
-				printf("%s", separator);
-		}
-		printf("\n");
-		va_end(ptr);
+		pointer = va_arg(ptr, unsigned int);
+		printf("%d", pointer);
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(ptr);
 }
