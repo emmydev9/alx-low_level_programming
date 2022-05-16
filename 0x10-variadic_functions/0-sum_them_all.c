9b950b2e05083fce08d0806c8536e93df419d7c4
Emmy <softdevcoder@gmail.com>
2022-05-16 16:32:43 -0400

    added 1-print_numbers.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(ptr, unsigned int);
+		sum += va_arg(ptr);
 
 	va_end(ptr);
 	return (sum);
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+* print_numbers - prints numbers with separator
+* @separator: char.
+* @n: number of arg.
+**/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, pointer;
+	va_list ptr;
+
+	if (n != 0)
+	{
+		va_start(ptr, n);
+		for (i = 0; i < n; i++)
+		{
+			pointer = va_arg(ptr, unsigned int);
+			printf("%d", pointer);
+			if (i != n - 1)
+				printf("%s", separator);
+		}
+		printf("\n");
+	}
+}
