ffe2e8d5afc08492847739c6bfff7cfd87d98514
Emmy <softdevcoder@gmail.com>
2022-05-24 17:42:34 -0400

    added 4-free_list.c file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+* free_list - free list.
+* @head: head node.
+**/
+void free_list(list_t *head)
+{
+	list_t *new_node;
+	while ((new_node = head) != NULL)
+	{
+		head = head->next;
+		free(new_node->str);
+		free(new_node);
+	}
+}
