62cf69c41194fec928aec97b39c80f7efe299e3e
Emmy <softdevcoder@gmail.com>
2022-05-26 15:29:46 -0400

    added 3-add_nodeint_end.c file

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
-* add_noteint_add - adds a new node at the end of a list.
+* add_nodeint_end - adds a new node at the end of a list.
 * @head: head of node.
 * @n: add data.
 * Return: node.
 **/
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *false_node;
