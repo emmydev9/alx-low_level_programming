08fb6c5e3ea6b5c3809177d2ad9904153f7228f5
Emmy <softdevcoder@gmail.com>
2022-05-26 13:49:18 -0400

    fix betty code and doc style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
-/*
-* print_listint - prints all the elements of a listint_t list
+/**
+* print_listint - prints all the elements of a linked list
 * @h: head of the list.
-* Return: size.
+* Return: number of elements.
 **/
 
 size_t print_listint(const listint_t *h)
