584f240888226ab22b3eff8464a9b8093b23874b
Emmy <softdevcoder@gmail.com>
2022-05-30 14:26:33 -0400

    [feature] added a function to check endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* get_endianness - checks the endianness.
+* Return: check
+**/
+
+int get_endianness(void)
+{
+	unsigned int x;
+	char *c;
+
+	x = 1;
+	c = (char *) &x;
+	return ((int) *c);
+}
