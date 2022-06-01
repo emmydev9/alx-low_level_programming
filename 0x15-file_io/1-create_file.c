d17c4bf4a0bc948073ab738687aad516775a3b72
Emmy <softdevcoder@gmail.com>
2022-06-01 17:31:15 -0400

    added a check for empty text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,6 +14,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (!text_content)
+		text_content = "";
 	if (fd == -1)
 		return (-1);
 	for (num_letters = 0; text_content[num_letters]; num_letters++)
